e164b46cfe98df52e2d1fc1b2dcb9f5eea4fae48
GuilhermePortella <guilhermeportella2@gmail.com>
2024-09-25 22:36:51 -0300

    Ajustes de criar conta

--- src/main/java/br/prafrentex_service/RegisterUserSimples/CadastroUsuarioService.java
+++ src/main/java/br/prafrentex_service/RegisterUserSimples/CadastroUsuarioService.java
@@ -1,6 +1,8 @@
 package br.prafrentex_service.RegisterUserSimples;
 
 import br.prafrentex_domain.Usuario;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -11,13 +13,35 @@ import java.util.Scanner;
 public class CadastroUsuarioService extends Usuario {
 
     private final Scanner scanner = new Scanner(System.in);
+    private List<Usuario> contasPF = new ArrayList<>();
 
     public void cadastrarUsuario() {
         Usuario usuario = coletarDadosUsuario();
-
         String agencia = gerarAgencia();
         String conta = gerarConta();
-        exibirDadosUsuario(usuario, agencia, conta);
+        adicionarContaPF(usuario, agencia, conta);
+    }
+
+    private void adicionarContaPF(Usuario usuario, String agencia, String conta) {
+        usuario.setAgencia(agencia);
+        usuario.setConta(conta);
+        contasPF.add(usuario);
+    }
+
+    public List<Usuario> criarListaContasPF() {
+        List<Usuario> listaContas = new ArrayList<>();
+        for (Usuario conta : contasPF) {
+            Usuario contaPF = new Usuario();
+            contaPF.setNome(conta.getNome());
+            contaPF.setSobrenome(conta.getSobrenome());
+            contaPF.setIdade(conta.getIdade());
+            contaPF.setEmail(conta.getEmail());
+            contaPF.setCpf(conta.getCpf());
+            contaPF.setAgencia(conta.getAgencia());
+            contaPF.setConta(conta.getConta());
+            listaContas.add(contaPF);
+        }
+        return listaContas;
     }
 
     private Usuario coletarDadosUsuario() {
@@ -41,14 +65,22 @@ public class CadastroUsuarioService extends Usuario {
         return new Usuario(nome, sobrenome, idade, email, cpf);
     }
 
-    private void exibirDadosUsuario(Usuario usuario, String agencia, String conta) {
-        System.out.println("\nUsuário cadastrado com sucesso!");
-        System.out.println("Nome: " + usuario.getNome() + " " + usuario.getSobrenome());
-        System.out.println("Idade: " + usuario.getIdade());
-        System.out.println("Email: " + usuario.getEmail());
-        System.out.println("CPF: " + usuario.getCpf());
-        System.out.println("Agência: " + agencia);
-        System.out.println("Conta: " + conta);
+    public void exibirDadosUsuarios() {
+        List<Usuario> listaUsuarios = criarListaContasPF();
+        if (listaUsuarios.isEmpty()) {
+            System.out.println("Nenhum usuário cadastrado.");
+        } else {
+            System.out.println("\nExibindo todos os usuários cadastrados:");
+            for (Usuario usuario : listaUsuarios) {
+                System.out.println("Nome: " + usuario.getNome() + " " + usuario.getSobrenome());
+                System.out.println("Idade: " + usuario.getIdade());
+                System.out.println("Email: " + usuario.getEmail());
+                System.out.println("CPF: " + usuario.getCpf());
+                System.out.println("Agência: " + usuario.getAgencia());
+                System.out.println("Conta: " + usuario.getConta());
+                System.out.println("-------------------------------");
+            }
+        }
     }
 
     private String gerarAgencia() {
@@ -59,5 +91,10 @@ public class CadastroUsuarioService extends Usuario {
     private String gerarConta() {
         Random random = new Random();
         return String.format("%09d", random.nextInt(1000000000));
+    }
+
+    public void processarCadastroEExibicao() {
+        cadastrarUsuario();
+        exibirDadosUsuarios();
     }
 }
