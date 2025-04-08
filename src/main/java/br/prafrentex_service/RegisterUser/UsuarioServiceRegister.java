2afd4539bb647c64f66ba99f950d493c25d930a6
GuilhermePortella <guilhermeportella2@gmail.com>
2025-04-07 22:39:26 -0300

    Pequenos ajustes

--- src/main/java/br/prafrentex_service/RegisterUser/UsuarioServiceRegister.java
+++ src/main/java/br/prafrentex_service/RegisterUser/UsuarioServiceRegister.java
@@ -2,6 +2,9 @@ package br.prafrentex_service.RegisterUser;
 
 //import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import br.prafrentex_domain.Usuario;
+
+
 
 /**
  *
@@ -15,7 +18,8 @@ public class UsuarioServiceRegister {
         //String senhaHash = passwordEncoder.encode(senha);
 
         // Criar o usuário e salvar no banco de dados com a senha criptografada
-        //Usuario usuario = new Usuario(nome, email, senhaHash);
+        
+        Usuario usuario = new Usuario(nome, email, senha, nome);
         // Associe o usuário a uma conta PF ou PJ
     }
 
